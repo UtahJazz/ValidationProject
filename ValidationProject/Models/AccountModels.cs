aef5eee4443e79c8cfdc32a883078067513507ff
UtahJazz <UtahJazz777@gmail.com>
2014-03-14 19:19:48 +0400

    First commit

---
+++ ValidationProject/Models/AccountModels.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace ValidationProject.Models
+{
+    public class RegisterModel
+    {
+
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение \"{0}\" должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare("Password", ErrorMessage = "Пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Электронная почта")]
+        [EmailAddress(ErrorMessage = "Неправильный адрес электронной почты")]
+        public string Email { get; set; }
+
+
+        [Required]
+        [Display(Name = "Дата Рождения")]
+        [DataType(DataType.Date)]
+        public DateTime Age { get; set; }
+
+        [Display(Name = "Настоящее имя")]
+        [DataType(DataType.Text)]
+        public string Realname { get; set; }
+
+        [Display(Name = "О себе")]
+        [DataType(DataType.MultilineText)]
+        public string UserInfo { get; set; }
+    }
+
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+
+    #region Не нужно
+    [Table("UserProfile")]
+    public class UserProfile
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+    }
+    #endregion
+}
